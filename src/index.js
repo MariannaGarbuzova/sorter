a00d7c9f215a9b7aa07f21201fee5e30e08b0ffc
MariannaGarbuzova <vesna09@tut.by>
2018-09-22 20:29:28 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
+
   constructor() {
-    // your implementation
+    this._arr = [];
+    this._comporator = (left, right) => left - right;
   }
 
   add(element) {
-    // your implementation
+    this._arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this._arr[index];
   }
 
   get length() {
-    // your implementation
+    return this._arr.length;
   }
 
   toArray() {
-    // your implementation
+    return [...this._arr];
   }
 
   sort(indices) {
-    // your implementation
+    // sort indecies first
+    indices.sort();
+
+    // bubble sort of inner arr
+    let foundUnsorted = true;
+    while (foundUnsorted) {
+      foundUnsorted = false;
+      for (let i = 0; i < indices.length - 1; i++) {
+        let a = this._arr[indices[i]];
+        let b = this._arr[indices[i + 1]];
+        if (this._comporator(a, b) > 0) {
+          this._arr[indices[i]] = b;
+          this._arr[indices[i + 1]] = a;
+          foundUnsorted = true;
+        }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this._comporator = compareFunction;
   }
 }
 
